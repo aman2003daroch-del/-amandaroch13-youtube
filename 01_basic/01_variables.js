b1d03bbf7314b779d8129fd12a21f55aa91ce069
Aman Daroch <aman2003daroch@gmail.com>
2025-10-07 17:00:30 +0000

    01_variables.js

---
+++ 01_basic/01_variables.js
@@ -0,0 +1,8 @@
+const accountNo =9458492242
+let accoutEmail ="fdse34@gmail.com"
+var accountPassward ="12345"
+accountCity ="una"
+
+accountPassward ="23456543"
+console.log(accountCity);
+console.table([accountNo,accoutEmail,accountPassward,accountCity]);
